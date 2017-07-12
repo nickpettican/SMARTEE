fa197f2d0e1abe294751f68cff900f713fae34d5
Nick Pettican <nicolas.pettican@postgrad.manchester.ac.uk>
2017-07-12 13:45:13 +0100

    aligned copyright blurb

--- run.py
+++ run.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# ___        SMARTEE V 0.0.1 by nickpettican			___
-# ___        Social Media Automated Research			___
-# ___        Tool for Evaluating Engagement				___
+# ___        SMARTEE V 0.0.1 by nickpettican		___
+# ___        Social Media Automated Research		___
+# ___        Tool for Evaluating Engagement		___
 
-# ___        Copyright 2017 Nicolas Pettican			___
+# ___        Copyright 2017 Nicolas Pettican		___
 
 # ___    This software is licensed under the Apache 2	___
-# ___    license. You may not use this file except in 	___
-# ___    compliance with the License.					___
-# ___    You may obtain a copy of the License at 		___
+# ___    license. You may not use this file except in	___
+# ___    compliance with the License.			___
+# ___    You may obtain a copy of the License at	___
 
-# ___    http://www.apache.org/licenses/LICENSE-2.0		___
+# ___    http://www.apache.org/licenses/LICENSE-2.0	___
 
 # ___    Unless required by applicable law or agreed	___
-# ___    to in writing, software distributed under		___
-# ___    the License is distributed on an "AS IS"		___
-# ___    BASIS, WITHOUT WARRANTIES OR CONDITIONS OF 	___
-# ___    ANY KIND, either express or implied. See the 	___
+# ___    to in writing, software distributed under	___
+# ___    the License is distributed on an "AS IS"	___
+# ___    BASIS, WITHOUT WARRANTIES OR CONDITIONS OF	___
+# ___    ANY KIND, either express or implied. See the	___
 # ___    License for the specific language governing	___
 # ___    permissions and limitations under the License.	___
 
